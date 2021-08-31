fcf8611a9b460818b4ebe2743c79570587abfe3b
Jake Itegboje <jakeitegsy@yahoo.com>
2021-08-31 13:34:20 -0500

    update setup_tdd.sh

--- setup_tdd.sh
+++ setup_tdd.sh
@@ -1,8 +1,18 @@
+testSetup=$(cat <<-ENDimport unittest
+
+class Test$($1)(unittest.TestCase):
+
+    def test_$projectName(self):
+        self.assertEqual(5, 6)
+END
+)
+
 directory=$1
 mkdir $directory
 cd $directory
 mkdir tests
 touch tests/__init__.py
+printf "%s" "$testSetup" > "tests/test_$($1).py"
 python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
